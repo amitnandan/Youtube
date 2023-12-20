67ab3837792e352ea49ed4868f29a97b7b385802
amitnandan <a.amitnandan@gmail.com>
2023-12-21 01:07:37 +0530

    added caching to search to reduce hit

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,15 +1,26 @@
 import React, { useEffect, useState } from "react";
 import { toggleMenu } from "../utils/appSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { YOUTUBE_SEARCH_API } from "../utils/constants";
+import { CacheResults } from "../utils/searchSlice";
 
 const Head = () => {
   const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const searchCache = useSelector((store) => store.search);
+  const dispatch = useDispatch();
+
   useEffect(() => {
     //api call after every key press
     //but if difference between 2 call is <200ms decline
-    const timer = setTimeout(() => getSearchSuggestions(), 200);
-
+    const timer = setTimeout(() => {
+      if (searchCache[searchQuery]) {
+        setSuggestions(searchCache[searchQuery]);
+      } else {
+        getSearchSuggestions();
+      }
+    }, 200);
     return () => {
       clearTimeout(timer);
     };
@@ -18,9 +29,14 @@ const Head = () => {
   const getSearchSuggestions = async () => {
     const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
     const json = await data.json();
-  };
 
-  const dispatch = useDispatch();
+    setSuggestions(json[1]);
+    dispatch(
+      CacheResults({
+        [searchQuery]: json[1],
+      })
+    );
+  };
 
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
@@ -28,48 +44,52 @@ const Head = () => {
 
   return (
     <div className="grid grid-flow-col p-5 m-2 shadow-lg">
-      <div className="flex  col-span-1">
+      <div className="flex col-span-1">
         <img
-          className="h-8 scale-150 mr-8 cursor-pointer"
           onClick={() => toggleMenuHandler()}
+          className="h-8 cursor-pointer"
           alt="menu"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRg6XmeytVztUaFJUj33AK5aORf5WDurpnW-w&usqp=CAU"
+          src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAARVBMVEX///8jHyAgHB0OBQgMAAWlpKQpJSaenZ309PUAAAAIAAD8/Pz5+fna2tqop6dvbW1oZmevrq4tKivFxMQYExRiYGC+vr7Dc4WrAAABB0lEQVR4nO3cS3LCMBAFQGIIIBPbhN/9jxqSyiIsTUnlydB9g1eSNV5MvdUKAAAAAAAAAAAAAAAAXtEwvscwDk3yHabSb2Loy/TRIOHUv8XRH+sHHMrSqR6U+hd1jHSE90P8lHC2/Lc0/0vzMy3WMdynxaFBwu+Jv4uh0cQHAAAAAAAAAIB59jG0ijdcT9sYTtcmK0PncumiuJRz/YD7bbf0ut4f3br+GvQt2PblrXrC3WbpUA/6sXrC/GeY/zvM/5aGmofHZiu0S//M/GoVDwAAAAAAAAAAZsjeuRerN1HL7hPy95fm76DNnzD/Lc3/0rxAJ3v+Xn0AAAAAAAAAAAAAAAD4T74AYhs1O+vt3ioAAAAASUVORK5CYII="
         />
-
-        <img
-          className="h-8 scale-150 "
-          alt="logo"
-          src="https://www.shutterstock.com/image-vector/youtube-logo-social-media-icon-260nw-2310134969.jpg"
-        />
+        <a href="/">
+          <img
+            className="h-8 mx-2"
+            alt="youtube-logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/2560px-YouTube_Logo_2017.svg.png"
+          />
+        </a>
       </div>
-
-      <div className="  col-span-10 px-10 scale-125">
+      <div className="col-span-10 px-10">
         <div>
           <input
-            className=" w-1/3 border border-gray-400 p-2 rounded-l-full "
+            className="px-5 w-1/2 border border-gray-400 p-2 rounded-l-full"
             type="text"
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
           />
           <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
             🔍
           </button>
         </div>
-        <div className="fixed bg-white py-2 px-5">
-          <ul>
-            <li>Iphone</li>
-            <li>Iphone</li>
-            <li>Iphone</li>
-            <li>Iphone</li>
-          </ul>
-        </div>
+        {showSuggestions && (
+          <div className="absolute bg-white py-2 px-2 w-[37rem] shadow-lg rounded-lg border border-gray-100">
+            <ul>
+              {suggestions.map((s) => (
+                <li key={s} className="py-2 px-3 shadow-sm hover:bg-gray-100">
+                  🔍 {s}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
-
       <div className="col-span-1">
         <img
-          className="h-8 scale-150"
+          className="h-8"
           alt="user"
-          src="https://t3.ftcdn.net/jpg/05/53/79/60/360_F_553796090_XHrE6R9jwmBJUMo9HKl41hyHJ5gqt9oz.jpg"
+          src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
         />
       </div>
     </div>
